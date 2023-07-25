2243ee0c17b067929050b64a801bf1f7e1880fd4
Dohyun Chung <pastapeter@naver.com>
2023-07-25 13:54:06 +0900

    [Feat] shapeInfoFactory 추가

---
+++ MyKeynote/MyKeynote/Factories/ShapeInfoFactory.swift
@@ -0,0 +1,31 @@
+//
+//  SlideFactory.swift
+//  MyKeynote
+//
+//  Created by Jung peter on 7/24/23.
+//
+
+import Foundation
+
+struct ShapeInfo: Equatable {
+  
+  static func == (lhs: ShapeInfo, rhs: ShapeInfo) -> Bool {
+    lhs.shape == rhs.shape
+  }
+  
+  var shape: Shape
+  var inspector: ShapeInspector
+}
+
+class ShapeInfoFactory {
+  
+  static func createShapeInfo(type: Shape.Type) -> ShapeInfo {
+    
+    let shapeFactory = ShapeFactory()
+    let inspectorFactory = InspectorFactory()
+    let shape = shapeFactory.makeShape(type: type.self)
+    return ShapeInfo(shape: shape, inspector: inspectorFactory.createInspector(shape: shape))
+  }
+  
+}
+
